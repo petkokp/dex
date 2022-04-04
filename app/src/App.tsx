2b34f11f1c43c1783ef7dbc88ac0e0266ca62f74
petkokpetkov <ppetko@softserveinc.com>
2022-04-04 21:12:27 +0300

    setup eslint for react app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,23 +3,25 @@ import Web3 from 'web3';
 
 function App() {
   const [account, setAccount] = useState<string>(); // state variable to set account.
-  
+
   useEffect(() => {
     async function load() {
       const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
       const accounts = await web3.eth.requestAccounts();
-      
+
       setAccount(accounts[0]);
     }
-    
+
     load();
-   }, []);
-  
-   return (
-     <div>
-       Your account is: {account}
-     </div>
-   );
+  }, []);
+
+  return (
+    <div>
+      Your account is:
+      {' '}
+      {account}
+    </div>
+  );
 }
 
 export default App;

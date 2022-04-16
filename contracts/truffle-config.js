81844be9a5b1cf19bca0391f038b5c1f718ac525
petkokpetkov <ppetko@softserveinc.com>
2022-04-16 10:53:34 +0300

    extended ci/cd to deploy to kovan network

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,8 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+const privateKey = process.env.PRIVATE_KEY;
+
+const endpoint = process.env.INFURA_ENDPOINT;
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 7545,
+      network_id: '*',
+    },
+    kovan: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () => new HDWalletProvider({
+        privateKeys: [privateKey],
+        providerOrUrl: endpoint,
+      }),
       network_id: '*',
     },
   },

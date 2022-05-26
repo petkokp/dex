5dc30832dd8254483057e6200d833cb940e8d61f
petkokpetkov <ppetko@softserveinc.com>
2022-05-27 00:00:45 +0300

    provide liquidity functionality

--- src/components/Pool/Pool.tsx
+++ src/components/Pool/Pool.tsx
@@ -2,45 +2,56 @@ import { useState } from 'react';
 import {
   Button,
   Container,
-  Select,
-  MenuItem,
-  SelectChangeEvent,
   TextField,
   Typography,
   IconButton,
 } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useSigner } from '../../hooks';
+import { Tokens } from '../tokens';
+import { LiquidityPoolService } from '../../services';
+import LP from '../../abis/LiquidityPool.json';
 
 export function Pool() {
-  const [firstToken, setFirstToken] = useState('ETH');
-  const [secondToken, setSecondToken] = useState('AAVE');
-
-  const [firstTokenValue, setFirstTokenValue] = useState(0);
-  const [secondTokenValue, setSecondTokenValue] = useState(0);
-
-  const changeFirstToken = (event: SelectChangeEvent) => {
-    setFirstToken(event.target.value as string);
-  };
-
-  const changeSecondToken = (event: SelectChangeEvent) => {
-    setSecondToken(event.target.value as string);
-  };
+  const [ethValue, setEthValue] = useState(0);
+  const [dexValue, setDexValue] = useState(0);
 
   const signer = useSigner();
 
-  const handleAddingLiquidity = () => {
-    const balance = signer?.getBalance();
-    if (balance && +balance >= firstTokenValue /* calculate gas somewhere */) {
-      signer?.sendTransaction?.({
-        to: '', // address of the contract
-        value: firstTokenValue,
-      });
+  const handleAddingLiquidity = async () => {
+    try {
+      const balance = await signer?.getBalance();
+
+      const chainId = signer?.provider?.network.chainId.toString();
+
+      const canAddLiquidity = chainId
+        && balance
+        && ethValue
+        && dexValue
+        && balance.gt(ethValue)
+        && balance.gt(dexValue);
+
+      if (canAddLiquidity) {
+        const contract = await LiquidityPoolService.getInstance().getLiquidityPoolContract({
+          abi: LP.abi,
+          address: (LP.networks as Record<string, Record<string, unknown>>)?.[
+            chainId
+          ].address as string,
+        });
+
+        const response = await contract?.deposit(ethValue, dexValue, {
+          gasLimit: 100000,
+        });
+
+        await response.wait();
+      }
+    } catch (error) {
+      console.error('Adding liquidity failed: ', error);
     }
   };
 
   return (
-    <Container maxWidth="sm">
+    <Container sx={{ display: 'flex', flexDirection: 'column' }} maxWidth="sm">
       <IconButton onClick={() => history.back()}>
         <ArrowBackIcon />
       </IconButton>
@@ -48,44 +59,44 @@ export function Pool() {
         Add liquidity
       </Typography>
       <Typography style={{ marginTop: 20, marginBottom: 20 }}>
-        Select first token
+        Select ETH value
       </Typography>
       <TextField
         label="Amount"
         type="number"
-        value={firstTokenValue}
-        onChange={(event) => setFirstTokenValue(Number(event.target.value))}
+        value={ethValue}
+        onChange={(event) => {
+          const value = Number(event.target.value);
+          if (value) setEthValue(value);
+        }}
         InputProps={{
-          endAdornment: (
-            <Select value={firstToken} onChange={changeFirstToken}>
-              <MenuItem value="ETH">ETH</MenuItem>
-              <MenuItem value="AAVE">AAVE</MenuItem>
-              <MenuItem value="USDT">USDT</MenuItem>
-            </Select>
-          ),
+          endAdornment: <Typography>{Tokens.ETH}</Typography>,
+          inputProps: { min: 0 },
         }}
         variant="outlined"
       />
       <Typography style={{ marginTop: 20, marginBottom: 20 }}>
-        Select second token
+        Select DEX value
       </Typography>
       <TextField
         label="Amount"
         type="number"
-        value={secondTokenValue}
-        onChange={(event) => setSecondTokenValue(Number(event.target.value))}
+        value={dexValue}
+        onChange={(event) => {
+          const value = Number(event.target.value);
+          if (value) setDexValue(value);
+        }}
         InputProps={{
-          endAdornment: (
-            <Select value={secondToken} onChange={changeSecondToken}>
-              <MenuItem value="ETH">ETH</MenuItem>
-              <MenuItem value="AAVE">AAVE</MenuItem>
-              <MenuItem value="USDT">USDT</MenuItem>
-            </Select>
-          ),
+          endAdornment: <Typography>{Tokens.DEX}</Typography>,
+          inputProps: { min: 0 },
         }}
         variant="outlined"
       />
-      <Button variant="contained" style={{ marginTop: 20 }} onClick={handleAddingLiquidity}>
+      <Button
+        variant="contained"
+        style={{ marginTop: 20 }}
+        onClick={handleAddingLiquidity}
+      >
         Add Liquidity
       </Button>
     </Container>

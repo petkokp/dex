621db97e2ec65f54c8b918776061be857b1b9a32
petkokpetkov <ppetko@softserveinc.com>
2022-06-10 22:51:12 +0300

    fixed lint errors

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -5,10 +5,9 @@ const LiquidityPool = artifacts.require('LiquidityPool');
 
 // eslint-disable-next-line func-names
 module.exports = async function (deployer) {
-
-  deployer.then(async function() {
-    let dexToken = await DexToken.new();
-    let amm = await AutomatedMarketMaker.new();
-    let lp = await LiquidityPool.new(dexToken.address, amm.address);
+  deployer.then(async () => {
+    const dexToken = await DexToken.new();
+    const amm = await AutomatedMarketMaker.new();
+    await LiquidityPool.new(dexToken.address, amm.address);
   });
 };

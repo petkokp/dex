1ac303ac7a16910d5086f1e0c13151295f7be31e
Dimitar Kostov <dimitar.kostov02@sap.com>
2022-06-14 22:26:59 +0300

    fix version && update

--- contracts/contracts/DexPair.sol
+++ contracts/contracts/DexPair.sol
@@ -1,47 +1,36 @@
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.10 <0.9.0;
+
+import "./DexToken.sol";
 
 contract DexPair {
-    address public token0; // getter && private
-    address public token1; // getter && private
-
-    uint112 private reserve0;
-    uint112 private reserve1;
-    uint32  private blockTimestampLast;
-    uint112 private totalSupply;
+    uint256 private reserve0;
+    uint256 private reserve1;
 
     event Mint(address indexed sender, uint amount0, uint amount1);
     event Sync(uint112 reserve0, uint112 reserve1);
 
-    constructor() public {
-        totalSupply = 1000000*100; //100 million
-    }
-
-    function mint(address to) external lock returns (uint liquidity) {
+    function mint(address to, uint256 totalSupply, uint256 balanceDexToken, uint256 balanceEth) external lock returns (uint liquidity) {
         (uint112 _reserve0, uint112 _reserve1,) = getReserves();
-        uint balance0 = IERC20(token0).balanceOf(address(this));
-        uint balance1 = IERC20(token1).balanceOf(address(this));
-        uint amount0 = balance0.sub(_reserve0);
-        uint amount1 = balance1.sub(_reserve1);
+        uint amount0 = balanceDexToken.sub(_reserve0);
+        uint amount1 = balanceEth.sub(_reserve1);
 
-        liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
+        liquidity = Math.min(amount0.mul(totalSupply) / _reserve0, amount1.mul(totalSupply) / _reserve1);
         require(liquidity > 0, 'INSUFFICIENT_LIQUIDITY_MINTED');
         _mint(to, liquidity);
 
-        _update(balance0, balance1, _reserve0, _reserve1);
+        _update(balanceDexToken, balanceEth, _reserve0, _reserve1);
         emit Mint(msg.sender, amount0, amount1);
     }
 
-    function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
-        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'Pancake: OVERFLOW');
-        reserve0 = uint112(balance0);
-        reserve1 = uint112(balance1);
-        blockTimestampLast = blockTimestamp;
-        emit Sync(reserve0, reserve1);
+    function _update(uint256 balanceDexToken, uint256 balanceEth, uint256 _reserve0, uint256 _reserve1) private {
+        require(balanceDexToken <= uint112(-1) && balanceEth <= uint112(-1), 'Pancake: OVERFLOW');
+        reserve0 = uint112(balanceDexToken);
+        reserve1 = uint112(balanceEth);
+        emit Sync(balanceDexToken, balanceEth);
     }
 
-    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1) {
         _reserve0 = reserve0;
         _reserve1 = reserve1;
-        _blockTimestampLast = blockTimestampLast;
     }
 }
